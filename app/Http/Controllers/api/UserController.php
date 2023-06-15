9169f430c701bca45889786f8946190e3453974a
druva <druva@netiapps.com>
2023-06-15 12:53:13 +0530

    login debug

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -51,7 +51,7 @@ class UserController extends Controller
 
         else {
             return response()->json([
-            'status' => 0,
+            'status' => "FALSE",
             'message' => 'Invalid credentials',
             'data' => $userdata
             
