79a35e023eeddefb034fb4e7a3aa2a2a6037ed19
druva <druva@netiapps.com>
2023-09-05 19:12:16 +0530

    temp files name replaced

--- resources/views/customer/list.blade.php
+++ resources/views/customer/list.blade.php
@@ -124,7 +124,7 @@
                 </form>
 
                     <div id="div2">
-                       <a target="_blank" href="{{ URL::to('/') }}/templates/HRE_Customers_template.xlsx" ><button class="btn btn-sm btn-light">Download Template</button></a>
+                       <a target="_blank" href="{{ URL::to('/') }}/templates/HRE_Customers_Template.xlsx" ><button class="btn btn-sm btn-light">Download Template</button></a>
                     </div>
               </div>
               
