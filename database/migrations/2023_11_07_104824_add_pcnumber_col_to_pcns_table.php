4a53a276206a2b0e38d25a8978dba71960699c61
druva <druva@netiapps.com>
2023-11-07 11:28:19 +0530

    pcn migration file added with pcnumber

--- database/migrations/2023_11_07_104824_add_pcnumber_col_to_pcns_table.php
+++ database/migrations/2023_11_07_104824_add_pcnumber_col_to_pcns_table.php
@@ -14,7 +14,7 @@ class AddPcnumberColToPcnsTable extends Migration
     public function up()
     {
         Schema::table('pcns', function (Blueprint $table) {
-            //
+            $table->integer('pcnumber');
         });
     }
 
