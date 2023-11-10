357826debdf17af4e77128ed30b9224a904cf418
druva <druva@netiapps.com>
2023-11-10 12:34:21 +0530

    debug

--- resources/views/indent/view_indents.blade.php
+++ resources/views/indent/view_indents.blade.php
@@ -50,14 +50,14 @@
           
           @if(Auth::user()->role_id == '1' or Auth::user()->role_id == '2')
             <div id="div2" style="margin-right: 30px">
-              <a data-bs-toggle="modal" data-bs-target="#triggerModal"  class="btn btn-light btn-outline-secondary" href="" title="<?php echo ($indents->settlement_triggerd == 'YES')? $indents->trigger_comments.'-'.$indents->commentor->employee_id:''  ?>"><label id="modal">Endores Settle</label></a>
+              <a data-bs-toggle="modal" data-bs-target="#triggerModal"  class="btn btn-light btn-outline-secondary" href="" title="<?php echo ($indents->settlement_triggerd == 'YES')? $indents->trigger_comments.'-'.$indents->commentor->employee_id:''  ?>"><label id="modal">Endorse Settle</label></a>
 
             </div>
           @endif
           
           @if(Auth::user()->role_id == '3' AND $indents->settlement_triggerd == 'YES')
             <div id="div2" style="margin-right: 30px">
-              <button class="btn btn-light btn-outline-secondary" title="<?php echo ($indents->settlement_triggerd == 'YES')? $indents->trigger_comments.'-'.$indents->commentor->employee_id:''  ?>" >Endore Comments</button>
+              <button class="btn btn-light btn-outline-secondary" title="<?php echo ($indents->settlement_triggerd == 'YES')? $indents->trigger_comments.'-'.$indents->commentor->employee_id:''  ?>" >Endorse Comments</button>
             </div>
           @endif
 
